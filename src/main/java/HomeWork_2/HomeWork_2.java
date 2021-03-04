a23022ad455a1527f93f0eaaecacf09c480b2447
RegisEmiel <regisemiel@yandex.ru>
2021-03-04 23:33:46 +0300

    Task 1 complete commit

--- src/main/java/HomeWork_2/HomeWork_2.java
+++ src/main/java/HomeWork_2/HomeWork_2.java
@@ -1,4 +1,29 @@
 package HomeWork_2;
 
+import java.util.Arrays;
+
 public class HomeWork_2 {
+    public static void task1() {
+        // Revers items of array
+        int[] a = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        System.out.print("Source array:\t");
+        System.out.println(Arrays.toString(a));
+
+        for (int i = 0; i < a.length; i++)
+            a[i] = a[i] ^ 1;
+
+        System.out.print("Result array:\t");
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Java 1 Homework 2");
+
+        //task 1
+        System.out.println("Task 1");
+        task1();
+        System.out.println();
+
+    }
 }
