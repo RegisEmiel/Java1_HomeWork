3979f00ed2283087bce75cf7206db771acf36fb2
RegisEmiel <regisemiel@yandex.ru>
2021-03-09 00:21:07 +0300

    Task 6 complete commit

--- src/main/java/HomeWork_2/HomeWork_2.java
+++ src/main/java/HomeWork_2/HomeWork_2.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class HomeWork_2 {
     public static void task1() {
-        // Revers items of array
+        // Изменить элементы массива на обратные
 
         int[] a = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 
@@ -19,9 +19,8 @@ public class HomeWork_2 {
         System.out.println(Arrays.toString(a));
     }
 
-    public static void task2()
-    {
-        // Init array elements with step 3
+    public static void task2() {
+        // Заполнить массив элементами с шагом 3
 
         int[] a = new int[8];
 
@@ -32,6 +31,8 @@ public class HomeWork_2 {
     }
 
     public static void task3() {
+        //Умножить на два элементы массива меньшие шести
+
         int[] a = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
 
         System.out.print("Исходный массив:\t");
@@ -46,6 +47,8 @@ public class HomeWork_2 {
     }
 
     public static void task4() {
+        // Заполнить диагонали матрицы единицами
+
         final int ROW_COUNT = 5;
 
         int[][] a = new int[ROW_COUNT][ROW_COUNT];
@@ -62,6 +65,8 @@ public class HomeWork_2 {
     }
 
     public static void task5() {
+        // Поиск минимального и максимального элемента
+
         final int ELEMENT_COUNT = 15;
         final int MAX_RANDOM_VALUE = 50;
 
@@ -69,9 +74,8 @@ public class HomeWork_2 {
 
         int[] a = new int[ELEMENT_COUNT];
 
-        for (int i = 0; i < ELEMENT_COUNT; i++) {
+        for (int i = 0; i < ELEMENT_COUNT; i++)
             a[i] = random.nextInt(MAX_RANDOM_VALUE);
-        }
 
         int minElement = a[0];
         int maxElement = a[0];
@@ -88,6 +92,44 @@ public class HomeWork_2 {
         System.out.println(Arrays.toString(a));
         System.out.printf("Минимальный элемент: %d\n", minElement);
         System.out.printf("Максимальный элемент: %d\n", maxElement);
+    }
+
+    // Функция определяет, есть ли граница в массиве с одинаковой суммой элементов левой и правой части
+    public  static  boolean borderExist(int[] a) {
+        int elementCount = a.length;
+
+        int leftSum = 0;
+        int rightSum;
+
+        for (int i = 0; i < elementCount - 1; i++) {
+            leftSum += a[i];
+
+            rightSum = 0;
+            for (int j = i + 1; j < elementCount; j++)
+                rightSum += a[j];
+
+            if (leftSum == rightSum)
+                return true;
+        }
+
+        return false;
+    }
+
+    public static void task6() {
+        int[] a1 = new int[] {6, 1, 2, 7, 2};
+        System.out.println("Массив: ");
+        System.out.println(Arrays.toString(a1));
+        System.out.println("Граница с одинаковой суммой элементов левой и правой части существует: " + borderExist(a1));
+
+        int[] a2 = new int[] {1, 1, 5};
+        System.out.println("Массив: ");
+        System.out.println(Arrays.toString(a2));
+        System.out.println("Граница с одинаковой суммой элементов левой и правой части существует: " + borderExist(a2));
+
+        int[] a3 = new int[] {1, 4, 5, 2, 2, 3, 3};
+        System.out.println("Массив: ");
+        System.out.println(Arrays.toString(a3));
+        System.out.println("Граница с одинаковой суммой элементов левой и правой части существует: " + borderExist(a3));
     }
 
     public static void main(String[] args) {
@@ -116,6 +158,11 @@ public class HomeWork_2 {
         //task 5
         System.out.println("Задание 5");
         task5();
+        System.out.println();
+
+        //task 6
+        System.out.println("Задание 6");
+        task6();
         System.out.println();
     }
 }
