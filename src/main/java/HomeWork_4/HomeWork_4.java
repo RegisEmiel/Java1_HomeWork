79add2efb91460d0ceeed6b76649b18def6c0916
RegisEmiel <regisemiel@yandex.ru>
2021-03-15 23:34:47 +0300

    Task 2, 4 complete commit

--- src/main/java/HomeWork_4/HomeWork_4.java
+++ src/main/java/HomeWork_4/HomeWork_4.java
@@ -25,6 +25,35 @@ public class HomeWork_4 {
         return false;
     }
 
+    public static boolean checkWin2(char symbol) {
+        int mainDiagonalWin = 0;
+        int sideDiagonalWin = 0;
+
+        for (int i = 0; i < SIZE; i++) {
+            int rowWin = 0;
+            int colWin = 0;
+
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == symbol)
+                    rowWin++;
+
+                if (map[j][i] == symbol)
+                    colWin++;
+            }
+
+            if (map[i][i] == symbol)
+                mainDiagonalWin++;
+
+            if (map[i][SIZE - i - 1] == symbol)
+                sideDiagonalWin ++;
+
+            if (rowWin == SIZE || colWin == SIZE || mainDiagonalWin == SIZE || sideDiagonalWin == SIZE)
+                return true;
+        }
+
+        return false;
+    }
+
     public static boolean isMapFull() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -44,6 +73,42 @@ public class HomeWork_4 {
         map[y][x] = DOT_O;
     }
 
+    public static void aiTurn2() {
+        int x = -1;
+        int y = -1;
+        boolean ai_win = false;
+        boolean flagWin = false;
+
+        for (int j = 0; j < SIZE; j++)
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                if (isCellValid(j, i))
+                {
+                    x = i;
+                    y = j;
+
+                    char tmp = map[i][j];
+                    map[i][j] = DOT_X;
+
+                    if (checkWin2(DOT_X))
+                        flagWin = true;
+
+                    map[i][j] = tmp;
+
+                    if (flagWin)
+                        break;
+                }
+            }
+
+            if (flagWin)
+                break;
+        }
+
+        System.out.println("Компьютер походил в точку " + (y + 1) + " " + (x + 1));
+        map[x][y] = DOT_O;
+    }
+
     public static void humanTurn() {
         int x, y;
         do {
@@ -55,8 +120,11 @@ public class HomeWork_4 {
     }
 
     public static boolean isCellValid(int x, int y) {
-        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
-        if (map[y][x] == DOT_EMPTY) return true;
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE)
+            return false;
+        if (map[y][x] == DOT_EMPTY)
+            return true;
+
         return false;
     }
 
@@ -85,12 +153,18 @@ public class HomeWork_4 {
     }
 
     public static void main(String[] args) {
+        System.out.println("Компьютер можно победить:");
+        System.out.println("1: (2, 2)");
+        System.out.println("2: (2, 3) | (1, 1)");
+        System.out.println("3: (1, 2) | (2, 1)");
+        System.out.println("4: (1, 3) | (3, 1)");
+
         initMap();
         printMap();
         while (true) {
             humanTurn();
             printMap();
-            if (checkWin(DOT_X)) {
+            if (checkWin2(DOT_X)) {
                 System.out.println("Победил человек");
                 break;
             }
@@ -98,9 +172,9 @@ public class HomeWork_4 {
                 System.out.println("Ничья");
                 break;
             }
-            aiTurn();
+            aiTurn2();
             printMap();
-            if (checkWin(DOT_O)) {
+            if (checkWin2(DOT_O)) {
                 System.out.println("Победил Искуственный Интеллект");
                 break;
             }
